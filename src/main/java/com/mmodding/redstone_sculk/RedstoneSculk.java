de2fb462f99a72c7da466f62f3bba00d4d15b30e
Max094Reikeb <maxencebekier@gmail.com>
2022-08-14 14:10:52 +0200

    Updated MModding Lib and added new frequency

--- src/main/java/com/mmodding/redstone_sculk/RedstoneSculk.java
+++ src/main/java/com/mmodding/redstone_sculk/RedstoneSculk.java
@@ -3,6 +3,7 @@ package com.mmodding.redstone_sculk;
 import com.mmodding.mmodding_lib.library.base.MModdingModContainer;
 import com.mmodding.mmodding_lib.library.base.MModdingModInitializer;
 import com.mmodding.mmodding_lib.library.initializers.ElementsInitializer;
+import com.mmodding.mmodding_lib.library.utils.GameEventUtils;
 import com.mmodding.redstone_sculk.init.BlockEntities;
 import com.mmodding.redstone_sculk.init.Blocks;
 import com.mmodding.redstone_sculk.init.GameEvents;
@@ -30,5 +31,7 @@ public class RedstoneSculk implements MModdingModInitializer {
 
 		// GameEvents
 		GameEvents.registerGameEvents();
+
+		GameEventUtils.putGameEventInFrequencies(GameEvents.REDSTONE_SCULK_SENSOR_ACTIVATE, 20);
 	}
 }
