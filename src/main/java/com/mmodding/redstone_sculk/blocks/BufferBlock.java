94300ced5a15d4461d171ff667bc880e38c53d74
Max094Reikeb <maxencebekier@gmail.com>
2022-08-15 15:44:29 +0200

    Fixed issues

--- src/main/java/com/mmodding/redstone_sculk/blocks/BufferBlock.java
+++ src/main/java/com/mmodding/redstone_sculk/blocks/BufferBlock.java
@@ -6,11 +6,14 @@ import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.TickPriority;
+import net.minecraft.world.World;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class BufferBlock extends RepeaterBlock implements BlockRegistrable, BlockWithItem {
@@ -18,6 +21,8 @@ public class BufferBlock extends RepeaterBlock implements BlockRegistrable, Bloc
 	private final AtomicBoolean registered = new AtomicBoolean(false);
 
 	private BlockItem item = null;
+
+	private int power;
 
 	public BufferBlock(AbstractBlock.Settings settings, boolean hasItem, ItemGroup itemGroup) {
 		this(settings, hasItem, itemGroup != null ? new QuiltItemSettings().group(itemGroup) : new QuiltItemSettings());
@@ -29,20 +34,34 @@ public class BufferBlock extends RepeaterBlock implements BlockRegistrable, Bloc
 	}
 
 	@Override
-	public int getWeakRedstonePower(BlockState state, BlockView world, BlockPos pos, Direction direction) {
-		if (!state.get(POWERED)) {
-			return 0;
-		} else {
-			return state.get(FACING) == direction ? getInputRedstone(state, world, pos, direction) : 0;
+	public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+		this.update(world, pos, state);
+		if (!this.isLocked(world, pos, state)) {
+			boolean bl = state.get(POWERED);
+			boolean bl2 = this.hasPower(world, pos, state);
+			if (bl && !bl2) {
+				world.setBlockState(pos, state.with(POWERED, Boolean.FALSE), 2);
+			} else if (!bl) {
+				world.setBlockState(pos, state.with(POWERED, Boolean.TRUE), 2);
+				if (!bl2) {
+					world.scheduleBlockTick(pos, this, this.getUpdateDelayInternal(state), TickPriority.VERY_HIGH);
+				}
+			}
+
 		}
 	}
 
-	private int getInputRedstone(BlockState state, BlockView world, BlockPos pos, Direction direction) {
+	private void update(World world, BlockPos pos, BlockState state) {
+		this.power = this.getPower(world, pos, state);
+	}
+
+	@Override
+	protected int getOutputLevel(BlockView world, BlockPos pos, BlockState state) {
 		if (this.isValidInput(state)) {
 			if (state.isOf(Blocks.REDSTONE_BLOCK)) {
 				return 15;
 			} else {
-				return state.isOf(Blocks.REDSTONE_WIRE) ? state.get(RedstoneWireBlock.POWER) : this.getStrongRedstonePower(state, world, pos, direction);
+				return state.isOf(Blocks.REDSTONE_BLOCK) ? state.get(RedstoneWireBlock.POWER) : this.power;
 			}
 		} else {
 			return 0;
